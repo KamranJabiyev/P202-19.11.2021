6c5ead4ec66179139d8ded3c42721736dfd0039d
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2021-11-19 18:36:48 +0400

    Add files via upload

---
+++ AdoNetEF/AdoNetEF/DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AdoNetEF.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoNetEF.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-3347ULG\SQLEXPRESS;" +
+                "Initial Catalog=P202CodeFirst;Integrated Security=true;");
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
